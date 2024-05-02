696a1f7577e1dd38747d1af163f9642bcf32e06f
mybess00 <betancourtrafael00@gmail.com>
2024-05-01 19:18:26 -0700

    update hook

--- hooks/usePrice.ts
+++ hooks/usePrice.ts
@@ -47,7 +47,7 @@ export function usePrice () {
 
   const convert = async (amount: string, from: string, to: string) => {
     const initialAmount = amount
-    if (from === "CUP" || from === "MLC" || to === "CUP" || to === "MLC") {
+    if (from === "CUP" || from === "MLC") {
       const responseCu = await fetch('api/coin/cu', {
         method: 'GET',
       })
@@ -56,53 +56,99 @@ export function usePrice () {
         throw new Error('Error 007: POST on hooks/usePrice.ts - CU coins')
       }
       const dataCu : DataElToque = await responseCu.json()
-      if (from === "CUP" || from === "MLC") {
-        if (from === "MLC") {
-          const newAmount = parseFloat(amount) * dataCu.data[0].tasas.MLC
-          amount = newAmount.toString()
+      if (from === "MLC") {
+        const newAmount = parseFloat(amount) * dataCu.data[0].tasas.MLC
+        amount = newAmount.toString()
+      }
+      if (to === "MLC" || to === "USD" || to === "EUR") {
+        to === "EUR" ? to = 'ECU' : to
+        const rate = 1 / dataCu.data[0].tasas[to as "USD" || "MLC" || "ECU"]
+        const result = parseFloat(amount) * rate
+        return {
+          "rate": from === "MLC" ? result / parseFloat(initialAmount) : rate, 
+          "request": {
+          amount,
+          from,
+          to,
+          },
+          result
         }
-        if (to === "MLC" || to === "USD" || to === "EUR") {
-          to === "EUR" ? to = 'ECU' : to
-          const rate = 1 / dataCu.data[0].tasas[to as "USD" || "MLC" || "ECU"]
-          const result = parseFloat(amount) * rate
-          return {
-            "rate": from === "MLC" ? result / parseFloat(initialAmount) : rate, 
-           "request": {
+      } else {
+        const amountUsd = parseFloat(amount) / dataCu.data[0].tasas.USD
+        const response = await fetch('api/convert', {
+          method: 'POST',
+          headers: { 'Content-Type' : 'application/json'},
+          body: JSON.stringify({
+            "amount": amountUsd.toString(),
+            from: "USD",
+            to
+          })
+        })
+        if (!response.ok) {
+          console.log(response)
+          throw new Error('Error 005: POST on hooks/usePrice.ts')
+        }
+        const data: ConvertReturn = await response.json()
+        const rate = data.data.result / parseFloat(amount)
+        return {
+          "rate": from === "MLC" ? data.data.result / parseFloat(initialAmount) : rate,
+          "request": {
             amount,
             from,
             to,
-           },
-           result
-          }
-        } else {
-          const amountUsd = parseFloat(amount) / dataCu.data[0].tasas.USD
-          const response = await fetch('api/convert', {
-            method: 'POST',
-            headers: { 'Content-Type' : 'application/json'},
-            body: JSON.stringify({
-              "amount": amountUsd.toString(),
-              from: "USD",
-              to
-            })
-          })
-          if (!response.ok) {
-            console.log(response)
-            throw new Error('Error 005: POST on hooks/usePrice.ts')
-          }
-          const data: ConvertReturn = await response.json()
-          const rate = data.data.result / parseFloat(amount)
-          return {
-            "rate": from === "MLC" ? data.data.result / parseFloat(initialAmount) : rate,
-            "request": {
-              amount,
-              from,
-              to,
-            },
-            "result": data.data.result
-          }
+          },
+          "result": data.data.result
         }
-      } 
-      
+      }
+    } else if (to === "CUP" || to === "MLC") {
+      const responseCu = await fetch('api/coin/cu', {
+        method: 'GET',
+      })
+      if (!responseCu.ok) {
+        console.log(responseCu)
+        throw new Error('Error 007: POST on hooks/usePrice.ts - CU coins')
+      }
+      const dataCu : DataElToque = await responseCu.json()
+      if (from === "MLC" || from === "USD" || from === "EUR") {
+        from === "EUR" ? from = 'ECU' : from
+        const rate = to === "MLC" ? dataCu.data[0].tasas[from as "ECU" | "MLC" | "USD"] / dataCu.data[0].tasas.MLC : dataCu.data[0].tasas[from as "ECU" | "MLC" | "USD"]
+        const result = parseFloat(amount) * rate
+        return {
+          "rate": from === "MLC" ? result / parseFloat(initialAmount) : rate, 
+          "request": {
+          amount,
+          from,
+          to,
+          },
+          result
+        }
+      } else {
+        const response = await fetch('api/convert', {
+          method: 'POST',
+          headers: { 'Content-Type' : 'application/json'},
+          body: JSON.stringify({
+            amount,
+            from,
+            to: "USD",
+          })
+        })
+        if (!response.ok) {
+          console.log(response)
+          throw new Error('Error 005: POST on hooks/usePrice.ts')
+        }
+        const data: ConvertReturn = await response.json()
+        const result = to === "MLC" ? data.data.result * (dataCu.data[0].tasas.USD / dataCu.data[0].tasas.MLC) : data.data.result * dataCu.data[0].tasas.USD
+        const rate = result / parseFloat(amount)
+        return {
+          rate,
+          "request": {
+            amount,
+            from,
+            to,
+          },
+          result
+        }
+      }
     } else {
       const response = await fetch('api/convert', {
         method: 'POST',
